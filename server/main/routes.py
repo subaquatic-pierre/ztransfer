352a25489635ddb1798258428fcbd0cd8c11d959
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-07-18 12:03:30 +0400

    Completed saving file on opload

--- server/main/routes.py
+++ server/main/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, current_app, redirect
+import os
+from flask.helpers import url_for
 from flask.json import jsonify
 
 main = Blueprint("main", __name__)
@@ -10,9 +12,12 @@ def home():
     return render_template("main/index.html")
 
 
-@main.route("/upload-files")
+@main.route("/upload-files", methods=["POST"])
 def upload_files():
-    print(request.data)
-    reply = {"status": "good job"}
+    if request.method == "POST":
+        files = request.files.getlist("files[]")
+        for file in files:
+            print(file)
+            file.save(os.path.join(current_app.root_path, "uploads", file.filename))
 
-    return jsonify(reply)
+    return redirect(url_for("main.home"))
