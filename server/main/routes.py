5faf0d593671c2dc095b0bdb14c36a71d35f8180
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-07-20 16:05:25 +0400

    Updated settings

--- server/main/routes.py
+++ server/main/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, current_app, redirect
 import os
-from flask.helpers import url_for
+import boto3
+from conf import aws_conf as conf
 from flask.json import jsonify
 
 main = Blueprint("main", __name__)
@@ -15,9 +16,21 @@ def home():
 @main.route("/upload-files", methods=["POST"])
 def upload_files():
     if request.method == "POST":
-        to_email = request.form.get("to_email")
-        from_email = request.form.get("from_email")
+        # to_email = request.form.get("to_email")
+        # from_email = request.form.get("from_email")
+        to_email = "subuatic.pierre@gmail.com"
+        from_email = "pierre@divesandybeach.com"
         message = request.form.get("message")
+
+        # Send email
+        ses_client = boto3.client("ses")
+        subject = "File Upload"
+        body_html = render_template(
+            "email/upload.html", data={to_email, from_email, message}
+        )
+        response = ses_client.send_email(
+            Destination={"ToAddresses": [to_email, from_email]}, Message={"Body": {}}
+        )
 
         print(f"From Email: {from_email} \n To Email: {to_email} \n Message: {message}")
 
