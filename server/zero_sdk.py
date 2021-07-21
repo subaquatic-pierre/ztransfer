683c89e062914b8d8cb4cb17b4fd1255a2d49e1e
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-07-21 11:28:24 +0400

    Started on get token method

--- server/zero_sdk.py
+++ server/zero_sdk.py
@@ -1,14 +1,18 @@
 import json
 import requests
+import hashlib
+from time import time
 from server.network_data import network_data
 from server.wallet_data import wallet_data
 
-print(wallet_data)
-
-
-BASE_URL = "https://beta.0chain.net/"
+TO_CLIENT_ID = "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d3"
+BASE_URL = "https://beta.0chain.net"
 WALLET_PUBLIC_KEY = wallet_data["Public_Key"]
 WALLET_ID = wallet_data["ID"]
+
+
+def pprint(res):
+    print(json.dumps(res.json(), indent=4))
 
 
 def make_request(url, method="GET", data=None, headers=None):
@@ -26,7 +30,7 @@ def make_request(url, method="GET", data=None, headers=None):
 def get_network_info():
     url = f"{BASE_URL}/dns/network"
     res = make_request(url)
-    print(json.dumps(res.json(), indent=4))
+    pprint(res)
 
 
 def create_wallet():
@@ -36,7 +40,7 @@ def create_wallet():
         # Build URL
         split = miner.split("/")
         miner_id = split[len(split) - 1]
-        url = f"{BASE_URL}{miner_id}/v1/client/put"
+        url = f"{BASE_URL}/{miner_id}/v1/client/put"
 
         # Build Data
         headers = {"Accept": "application/json", "Content-Type": "application/json"}
@@ -48,13 +52,50 @@ def create_wallet():
         }
 
         # Make request
-        res = make_request(url, data, method="PUT", headers=headers)
+        res = make_request(url, method="PUT", data=data, headers=headers)
         results.append(res)
 
-    print(results)
+    for res in results:
+        print(res.text)
 
 
 def get_balance():
-    url = f"{BASE_URL}sharder01/v1/client/get/balance?client_id={WALLET_ID}"
+    url = f"{BASE_URL}/sharder01/v1/client/get/balance?client_id={WALLET_ID}"
     res = make_request(url)
-    print(res)
+    pprint(res)
+
+
+def add_tokens(num_tokens):
+    url = f"{base_url}/miner01/v1/transaction/put"
+    headers = {"content-type: application/json; charset=utf-8"}
+
+    # Creation date
+    creation_date = int(time())
+
+    # Transaction data hash
+    transaction_data_hash = hashlib.sha3_256()
+    transaction_data_hash.update(b'{"name":"pour","input":{},"name":null}')
+
+    hash_array = [
+        creation_date,
+        WALLET_ID,
+        TO_CLIENT_ID,
+        10000000000,
+        transaction_data_hash,
+    ]
+
+    # Build raw data
+    data = {
+        "hash": "{{hash_of_request_data}}",
+        "signature": "{{signature}}",
+        "version": "1.0",
+        "client_id": wallet_id,
+        "creation_date": creation_date,
+        "to_client_id": "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d3",
+        "transaction_data": '{"name":"pour","input":{},"name":null}',
+        "transaction_fee": 0,
+        "transaction_type": 1000,
+        "transaction_value": 10000000000,
+        "txn_output_hash": "",
+        "public_key": wallet_public_key,
+    }
