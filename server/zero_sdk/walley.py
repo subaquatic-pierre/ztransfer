523a3952e578c286d9ef7af7905939a7aa5a747e
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-07-23 14:11:55 +0400

    Create return response method on wallet

--- server/zero_sdk/walley.py
+++ server/zero_sdk/walley.py
@@ -7,16 +7,19 @@ from zero_sdk.utils import get_home_path, network_url_from_dns_path
 
 default_wallet = {}
 default_network = {}
+network_url = ""
 
 with open(f"{get_home_path()}/.zcn/wallet.json", "r") as f:
     default_wallet = json.loads(f)
 
 with open(f"{get_home_path()}/.zcn/wallet.json", "r") as f:
     default_network = yaml.safe_load(f)
+    if default_network["block_worker"]:
+        network_url = network_url_from_dns_path(default_network["block_worker"])
 
 
 class Wallet:
-    def __init__(self, config=default_wallet, network=default_network, default=True):
+    def __init__(self, config=default_wallet, network_url=network_url, default=True):
         if default == True:
             self.client_id = config["client_id"]
             self.client_key = config["client_key"]
@@ -25,30 +28,25 @@ class Wallet:
             self.mnemonics = config["mnemonics"]
             self.version = config["version"]
             self.date_created = config["date_created"]
-            try:
-                self.network_url = network_url_from_dns_path(network["block__worker"])
-                self.network_config = default_network
-            except KeyError as e:
-                raise KeyError("Unsupported Key for wallet config")
+            self.network_url = network_url
 
     def get_network_info(self):
         url = f"{self.network_url}/dns/network"
         res = requests.get(url)
-        if res.status_code == 200:
-            return res.json()
-        else:
-            return f"An error occured fetching network info: {res.text}"
+        error_message = f"An error occured fetching network info: {res.text}"
+        self.return_response(res, error_message)
 
     def get_balance(self):
-        url = f"{this}/sharder01/v1/client/get/balance?client_id={WALLET_ID}"
+        url = f"{self.network_url}/sharder01/v1/client/get/balance?client_id={self.client_id}"
         res = requests.get(url)
-        pprint(res)
-        url = f"{self.network_url}/dns/network"
-        res = requests.get(url)
+        error_message = f"An error occured getting network info: {res.text}"
+        self.return_response(res, error_message)
+
+    def return_response(self, res, error_message):
         if res.status_code == 200:
             return res.json()
         else:
-            return f"An error occured getting network info: {res.text}"
+            return error_message
 
     def __repr__(self):
         return f"Wallet(config={default_wallet}, default=True/False)"
