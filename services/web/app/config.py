e1ba82ea43349b4916afdb8404a19bbc0957f88e
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-07-31 08:23:01 +0400

    Fixed email problems

--- services/web/app/config.py
+++ services/web/app/config.py
@@ -1,16 +1,27 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+def use_dot_env(var_name):
+    value = os.environ.get(var_name)
+    print(value)
+    return value
+
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", use_dot_env("SECRET_KEY"))
 
     # Mail Settings
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_SERVER = os.getenv("MAIL_SERVER", use_dot_env("MAIL_SERVER"))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", use_dot_env("MAIL_USERNAME"))
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", use_dot_env("MAIL_PASSWORD"))
+    MAIL_DEFAULT_SENDER = os.getenv(
+        "MAIL_DEFAULT_SENDER", use_dot_env("MAIL_DEFAULT_SENDER")
+    )
     MAIL_USE_TLS = True
 
     # Database settings
